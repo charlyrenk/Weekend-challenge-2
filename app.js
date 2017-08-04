51aa4f3fd37e6f363290b18af5ee20e5fa742e97
Charles Renk <renkx008@umn.edu>
2017-08-04 16:44:48 -0500

    Synced client side input to server side using POST

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.post('/calc', function(req, res) {
+	console.log('message post was hit!')
+    res.sendStatus(201);
+});
+
 app.listen(port, function() {
 	console.log('listening on port', port);
 });
--- public/client.js
+++ public/client.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('Jquery is sourced')
+    $('#addButton').on('click', function () {
+        console.log('add button was clicked')
+        var numberX = $("#numberX").val();
+        var numberY = $("#numberY").val();
+        var type = "Add"
+        $.ajax({
+            method: 'POST',
+            url: '/calc',
+            // data becomes req.body
+            data: {
+                numberX: numberX,
+                numberY: numberY,
+            },
+            success: function (response) {
+                console.log(response);
+            }
+        })
+    })
 })
